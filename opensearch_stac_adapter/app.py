94ede5433659f5d4c451429294e534f698c7bbf0
Stijn Caerts <stijn.caerts@vito.be>
2022-09-20 11:13:53 +0200

    GDD-2207: patch custom OpenAPI for use behind proxy

--- opensearch_stac_adapter/app.py
+++ opensearch_stac_adapter/app.py
@@ -1,10 +1,12 @@
 from stac_fastapi.api.app import StacApi
 from stac_fastapi.api.app import ApiSettings
 from fastapi import FastAPI, Request
+from fastapi.openapi.utils import get_openapi
 from asgi_logger import AccessLoggerMiddleware
 from opensearch_stac_adapter.adapter import OpenSearchAdapterClient
 from opensearch_stac_adapter.models.search import AdaptedSearch
 import logging
+from typing import Optional, Dict, Any
 
 settings = ApiSettings()
 
@@ -24,6 +26,22 @@ app.add_middleware(
     format='%(t)s %(client_addr)s "%(request_line)s" %(s)s %(B)s %(M)s',
     logger=logging.getLogger("access")
 )
+
+
+def customize_openapi() -> Optional[Dict[str, Any]]:
+    """Customize OpenAPI schema."""
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title=api.title, version=api.api_version, routes=app.routes, servers=app.servers
+    )
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = customize_openapi
 
 
 @app.middleware("http")
